a2eedaaeb685a4707c6dc5212a43df5612c70349
Luis <luismaglz@live.com>
2021-07-09 22:44:13 -0600

    initial setup

---
+++ games/spring-challenge.ts
@@ -0,0 +1,6 @@
+declare function readline():string;
+
+const numberOfCells: number = parseInt(readline()); // 37
+for(let i = 0; i < numberOfCells; i++){
+    const inputs = readline().split(' ');
+}
