82111a5bbd14f86549662843597dfdd57b9bf497
Luis <luismaglz@live.com>
2021-07-11 22:48:21 -0600

    chianges

--- games/spring-challenge.ts
+++ games/spring-challenge.ts
@@ -1,5 +1,23 @@
-declare function readline():string;
+declare function readline(): string;
 
+
+interface GameState {
+    day: number; // 0 -> 23 // 0 first day, 23 last day
+    nutrients: number; // the base score you gain from the next COMPLETE action
+    sun: number; // your sun points
+    score: number; // your current score
+    oppSun: number; // opponent's sun points
+    oppScore: number; // opponent's score
+    oppIsWaiting: boolean; // whether your opponent is asleep until the next day
+    numberOfTrees: number; // the current amount of trees
+}
+
+interface Tree {
+    cellIndex:number; // location of this tree
+    size:number; // size of this tree: 0-3
+    isMine:boolean; // 1 if this is your tree
+    isDormant:boolean; // 1 if this tree is dormant
+}
 /**
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
@@ -7,7 +25,7 @@ declare function readline():string;
 
 const numberOfCells = parseInt(readline()); // 37
 for (let i = 0; i < numberOfCells; i++) {
-    var inputs = readline().split(' ');
+    const inputs = readline().split(' ');
     const index = parseInt(inputs[0]); // 0 is the center cell, the next cells spiral outwards
     const richness = parseInt(inputs[1]); // 0 if the cell is unusable, 1-3 for usable cells
     const neigh0 = parseInt(inputs[2]); // the index of the neighbouring cell for each direction
@@ -30,22 +48,52 @@ while (true) {
     const oppScore = parseInt(inputs[1]); // opponent's score
     const oppIsWaiting = inputs[2] !== '0'; // whether your opponent is asleep until the next day
     const numberOfTrees = parseInt(readline()); // the current amount of trees
+
+
+    const trees: Tree[] = [];
     for (let i = 0; i < numberOfTrees; i++) {
         var inputs = readline().split(' ');
         const cellIndex = parseInt(inputs[0]); // location of this tree
         const size = parseInt(inputs[1]); // size of this tree: 0-3
         const isMine = inputs[2] !== '0'; // 1 if this is your tree
         const isDormant = inputs[3] !== '0'; // 1 if this tree is dormant
+        trees.push({
+            cellIndex,
+            size,
+            isMine,
+            isDormant
+        })
     }
     const numberOfPossibleActions = parseInt(readline()); // all legal actions
     for (let i = 0; i < numberOfPossibleActions; i++) {
         const possibleAction = readline(); // try printing something from here to start with
+        // console.log(possibleAction);
     }
 
     // Write an action using console.log()
     // To debug: console.error('Debug messages...');
+    const gameState: GameState = {
+        day,
+        nutrients,
+        sun,
+        score,
+        oppSun,
+        oppScore,
+        oppIsWaiting,
+        numberOfTrees,
+    }
+
+    log(gameState);
 
 
     // GROW cellIdx | SEED sourceIdx targetIdx | COMPLETE cellIdx | WAIT <message>
     console.log('WAIT');
 }
+
+function log(obj: any): void {
+    console.error(obj);
+}
+function GROW(): void { };
+function SEED(): void { };
+function COMPLETE(): void { }
+function WAIT(): void { }
